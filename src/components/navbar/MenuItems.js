08dc36a73e591c1a8740e4c4c2dff95c049d7dc9
Slawomir-J <nonseyy@gmail.com>
2022-05-05 13:00:23 +0200

    Added Navbar component

---
+++ src/components/navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: 'My Posts',
+    path: '/my-posts',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Profile',
+    path: '/my-profile',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'All post',
+    path: '/all-posts',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Contact Us',
+    path: '/contact-us',
+    cName: 'dropdown-link'
+  }
+];
